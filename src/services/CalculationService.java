0f92738824dcf9e3d1cf1ffe6147b45021291cb4
Nelio Alves <acenelio@gmail.com>
2018-06-22 15:34:17 -0300

    Bounded generics implementation

--- src/services/CalculationService.java
+++ src/services/CalculationService.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public class CalculationService {
 
-	public static Integer max(List<Integer> list) {
+	public static <T extends Comparable<T>> T max(List<T> list) {
 		if (list.isEmpty()) {
 			throw new IllegalStateException("List can't be empty");
-		}
-		Integer max = list.get(0);
-		for (Integer item : list) {
+		} 
+		T max = list.get(0);
+		for (T item : list) {
 			if (item.compareTo(max) > 0) {
 				max = item;
 			}
